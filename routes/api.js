7a64d0f43e5a806b14067539191a8fd474eb46bf
trongbinh15 <trongbinhnguyen15@gmail.com>
2018-11-06 20:11:33 +0700

    chinh sua title

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@
 var data = {
   "posts": [
     {
-      "title": "Lorem ipsum",
+      "title": "Post 1",
       "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
